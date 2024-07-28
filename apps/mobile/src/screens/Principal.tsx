caee9936afc7ef4719e4ff01434cc3be06aee204
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-07-28 15:10:17 -0300

    feat: add tabs navigation

--- apps/mobile/src/screens/Principal.tsx
+++ apps/mobile/src/screens/Principal.tsx
@@ -1,9 +1,69 @@
-import { Text, View } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { StyleSheet, Text, View } from 'react-native'
+import Home from './Home'
+import Agendamento from './Agendamento'
+import Usuario from './Usuario'
+import Icone from '../components/shared/Icone'
 
-export default function Principal() {
+const Tab = createBottomTabNavigator()
+
+export default function Principal({ navigation }: any) {
+  function tab(nome: string, componente: any, label: string, icone: string) {
+    return (
+      <Tab.Screen
+        name={nome}
+        component={componente}
+        options={{
+          unmountOnBlur: true,
+          tabBarIcon: ({ focused }) => (
+            <View style={styles.tabScreen}>
+              <Icone
+                nome={icone as any}
+                tamanho={24}
+                color={focused ? '#29A7EA' : '#9DA2AE'}
+              />
+              <Text
+                style={{
+                  ...styles.tabScreenText,
+                  color: focused ? '#29A7EA' : '#9DA2AE',
+                }}
+              >
+                {label}
+              </Text>
+            </View>
+          ),
+        }}
+      />
+    )
+  }
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Principal</Text>
-    </View>
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveBackgroundColor: '#222',
+        tabBarInactiveBackgroundColor: '#222',
+        tabBarStyle: {
+          backgroundColor: '#222',
+        },
+      }}
+    >
+      {tab('Home', Home, 'Home', 'home-outline')}
+      {tab('Agendamento', Agendamento, 'Agendamento', 'calendar-outline')}
+      {tab('Usuario', Usuario, 'Usuário', 'person-outline')}
+    </Tab.Navigator>
   )
 }
+
+const styles = StyleSheet.create({
+  tabScreen: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  tabScreenText: {
+    fontSize: 10,
+  },
+})
